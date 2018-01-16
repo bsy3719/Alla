f2c55cb9cddddb14892b94169728becc749fdc13
bsy3719 <bsy3719@naver.com>
2018-01-16 11:21:11 +0900

    최종 수정

---
+++ app/src/main/java/com/fave/android/alla/activity/CardNewsWriteActivity.java
@@ -0,0 +1,15 @@
+package com.fave.android.alla.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.fave.android.alla.R;
+
+public class CardNewsWriteActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_news_write);
+    }
+}
